d88895718076dc68cfb5614048fbffc5514c92de
Rythm Sanghvi <sanghvi.rythm7402@gmail.com>
2026-07-20 00:26:13 -0700

    Frontend Update

--- index.js
+++ index.js
@@ -34,10 +34,6 @@ app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World</h1>")
-})
-
 app.get("/api/notes", (request, response) => {
   response.json(notes)
 })
